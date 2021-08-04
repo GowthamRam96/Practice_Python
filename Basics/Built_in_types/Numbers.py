38b5de30414ae47ce39f689d04949b6d062f59ed
Gowtham Ramarajan <84629235+GowthamRam96@users.noreply.github.com>
2021-08-04 22:40:35 +0530

    Numbers

---
+++ Basics/Built_in_types/Numbers.py
@@ -0,0 +1,32 @@
+#addition
+a=121+253
+print(a)
+
+#multiplication
+a=1.5*4
+print(a)
+
+#exponential
+a=2**10
+print(a)
+
+#length
+a=len(str(5**10000))
+print(a)
+
+#String multiplication
+print(5.5*12)
+
+#in built math functions
+import math
+pi = math.pi
+print(pi)
+
+val = math.sqrt(50)
+print(val)
+
+#random results
+import random
+print(random.random())
+
+print(random.choice([1,2,3,4]))
